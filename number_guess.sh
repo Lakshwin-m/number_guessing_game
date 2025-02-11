9a2cec63872f8a56245208e25500dedb3c623bd9
Lakshwin krishna <lakshwinsujatha2@gmail.com>
2025-02-11 12:20:56 +0000

    refactor: menu is made into function

--- number_guess.sh
+++ number_guess.sh
@@ -20,3 +20,20 @@ GUESS() {
 }
 
 MENU
+
+while [[ -z $USERNAME ]]; do
+    MENU "The username filed is required."
+done
+
+# --- Find user by name ----
+USER=$($PSQL "SELECT username,games_played,best_game FROM users WHERE username='$USERNAME'")
+if [[ -z $USER ]]; then
+    INSERT_USER=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
+    echo "Welcome, $USERNAME! It looks like this is your first time here."
+else
+    IFS='|' read -r -a USER_ARRAY <<<"$USER"
+    for ((i = 0; i <= ${#USER_ARRAY[@]} - 1; i++)); do
+        USER_ARRAY[$i]=$(echo ${USER_ARRAY[$i]} | sed -e 's/^+ | +$//')
+    done
+    echo "Welcome back, ${USER_ARRAY[0]}! You have played ${USER_ARRAY[1]} games, and your best game took ${USER_ARRAY[2]} guesses."
+fi
