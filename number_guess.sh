7594e8a48f141b2c1d782a51069b3ac8e1cce16d
Lakshwin krishna <lakshwinsujatha2@gmail.com>
2025-02-11 12:19:15 +0000

    feat: menu added

--- number_guess.sh
+++ number_guess.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-echo 
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+
+MENU() {
+    if [[ $1 ]]; then
+        echo -e "$1\n"
+    fi
+    echo "Enter your username:"
+    read USERNAME
+}
+
+GUESS() {
+    if [[ $1 ]]; then
+        echo -e "$1"
+    else
+        echo -e "Guess the secret number between 1 and 1000:"
+    fi
+    read NUMBER
+}
+
+MENU
